4d8fedad72a596545574e6b04fd4e29eb4a10b1f
JEvangelista-XLR <jarron.evangelista@education.nsw.gov.au>
2026-03-18 02:34:13 +0000

    restarted new repo once again...

---
+++ Booking-Service-&-Co-App/booking-service-&-co.py
@@ -0,0 +1,14 @@
+import os
+from flask import  Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST",])
+def page_redirect():
+    if request.method == "GET":
+        print("#")
+
+    return render_template("frontendlanding.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
