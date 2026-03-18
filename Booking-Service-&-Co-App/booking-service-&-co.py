c0d67479903aad562b999a4b9d5e4b6bff3e934d
JEvangelista-XLR <jarron.evangelista@education.nsw.gov.au>
2026-03-18 03:42:01 +0000

    continuing to allign button

--- Booking-Service-&-Co-App/booking-service-&-co.py
+++ Booking-Service-&-Co-App/booking-service-&-co.py
@@ -3,12 +3,12 @@ from flask import  Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def page_redirect():
-    if request.method == "GET":
-        print("#")
-
+@app.get("/")
+def home_page():
     return render_template("frontendlanding.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
