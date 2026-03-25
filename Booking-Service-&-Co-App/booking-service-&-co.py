46fa848e0bff71237edfd1ab1ed8902e416fb43b
JEvangelista-XLR <jarron.evangelista@education.nsw.gov.au>
2026-03-25 03:16:08 +0000

    added doc string for explaining functions

--- Booking-Service-&-Co-App/booking-service-&-co.py
+++ Booking-Service-&-Co-App/booking-service-&-co.py
@@ -1,19 +1,20 @@
+"""Python code for links to webpages within website"""
 import os
 from flask import  Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.get("/")
-def home_page():
+@app.get("/") #"/" sets home page as root page
+def home_page(): # home page render
     return render_template("frontendlanding.html")
 
 @app.get("/about")
-def about_page():
+def about_page(): # about page render
     return render_template("aboutpage.html")
 
 @app.get("/contacts")
-def contacts_page():
+def contacts_page(): # contacts page render
     return render_template("contactspage.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # TURN OFF DEBUG WHEN LIVE
